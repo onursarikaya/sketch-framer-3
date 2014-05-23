aacccb6dd39cc332f28896e8ae96cd6701a021e7
Ale Muñoz <bomberstudios@gmail.com>
2014-05-23 17:39:18 +0200

    Nightly → Beta

--- export.rb
+++ export.rb
@@ -4,7 +4,7 @@ require 'tempfile'
 # filename = ARGV[0] # not used, we'll just export the frontmost document
 
 tmp_file = Tempfile.new('framer-generator')
-tmp_file.write "[[[COScript app:'Sketch Nightly'] delegate] runPluginAtURL:[NSURL fileURLWithPath:'Export to Framer.sketchplugin']]"
+tmp_file.write "[[[COScript app:'Sketch Beta'] delegate] runPluginAtURL:[NSURL fileURLWithPath:'Export to Framer.sketchplugin']]"
 tmp_file.rewind
 
 cmd = "bin/coscript #{tmp_file.path} 2>/dev/null"
--- src/run.js
+++ src/run.js
@@ -1,3 +1,3 @@
-// [[[COScript app:"Sketch Nightly"] delegate] runPluginScript:"print(doc)"]
-// [[[COScript app:"Sketch Nightly"] delegate] runPluginAtURL:[NSURL fileURLWithPath:"Log.sketchplugin"]]
-[[[COScript app:"Sketch Nightly"] delegate] runPluginAtURL:[NSURL fileURLWithPath:"Export to Framer.sketchplugin"]]
+// [[[COScript app:"Sketch Beta"] delegate] runPluginScript:"print(doc)"]
+// [[[COScript app:"Sketch Beta"] delegate] runPluginAtURL:[NSURL fileURLWithPath:"Log.sketchplugin"]]
+[[[COScript app:"Sketch Beta"] delegate] runPluginAtURL:[NSURL fileURLWithPath:"Export to Framer.sketchplugin"]]

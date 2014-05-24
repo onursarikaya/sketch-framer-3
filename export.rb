709b23c273f2072ef9bde17be81dddc527c46b15
Ale Muñoz <bomberstudios@gmail.com>
2014-05-24 15:49:11 +0200

    Simplify export command (thanks a lot, @uhunkler :)

--- export.rb
+++ export.rb
@@ -1,15 +1,2 @@
 #!/usr/bin/env ruby
-require 'tempfile'
-
-# filename = ARGV[0] # not used, we'll just export the frontmost document
-
-tmp_file = Tempfile.new('framer-generator')
-tmp_file.write "[[[COScript app:'Sketch Beta'] delegate] runPluginAtURL:[NSURL fileURLWithPath:'Export to Framer.sketchplugin']]"
-tmp_file.rewind
-
-cmd = "bin/coscript #{tmp_file.path} 2>/dev/null"
-
-system(cmd)
-
-tmp_file.close
-tmp_file.unlink
+system("bin/coscript -e '[[[COScript app:\"Sketch Beta\"] delegate] runPluginAtURL:[NSURL fileURLWithPath:\"#{__dir__}/Export to Framer.sketchplugin\"]]'")

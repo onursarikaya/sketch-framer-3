06a0911b2e10509421d04ca68322b3ee309a3d38
Koen Bok <koen@madebysofa.com>
2014-07-11 16:39:45 +0200

    Native

--- src/run.js
+++ src/run.js
@@ -1,6 +1,21 @@
-scriptData = [NSString stringWithContentsOfFile:"Export to Framer.sketchplugin" encoding:NSUTF8StringEncoding error:nil];
-
-sketchApp = [COScript app:"Sketch Beta"];
+var sketchApp = [COScript app:"Sketch Beta"];
 sketchApp = sketchApp || [COScript app:"Sketch"];
+
+var sketchVersion = 0;
+var bundles = [NSBundle allBundles]
+for (var i = 0; i < [bundles count]; i++) {
+  var bundle = [bundles objectAtIndex:i]
+  if([bundle resourcePath].indexOf("Sketch")) {
+    sketchVersion = 0 + parseInt([bundle infoDictionary]["CFBundleVersion"]);
+  }
+}
+
+if (sketchVersion >= 8053) {
+  log("Using the native Framer Exporter")
+  scriptData = "var path = [doc exportFramer];log('TEMP_DIR:'+path);";
+} else {
+  log("Using the CocoaScript Framer Exporter")
+  scriptData = [NSString stringWithContentsOfFile:"Export to Framer.sketchplugin" encoding:NSUTF8StringEncoding error:nil];
+}
 
 [[sketchApp delegate] runPluginScript:scriptData];
